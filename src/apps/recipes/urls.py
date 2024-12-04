2a0e82169984e59371acee5f8d41df7f2318fa7b
Joud <joud.hasan85@gmail.com>
2024-12-04 20:03:39 +0200

    Protecting a Class-Based View (CBV)

--- src/apps/recipes/urls.py
+++ src/apps/recipes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import home, recipe_list, recipe_detail
 
+
 app_name = 'recipes'
 
 urlpatterns = [
--- src/apps/recipes/views.py
+++ src/apps/recipes/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required  # For FBV authentication
 from .models import Recipe  # Import the Recipe model
 
+# Function-Based Views (FBVs)
 def home(request):
+    """Render the homepage."""
     return render(request, 'recipes/recipes_home.html')
 
+# Protect recipe_list with @login_required
+@login_required
 def recipe_list(request):
+    """Render the list of recipes."""
     # Fetch all recipes from the database
     recipes = Recipe.objects.all()
     return render(request, 'recipes/recipes_list.html', {'recipes': recipes})
 
+# Protect recipe_detail with @login_required
+@login_required
 def recipe_detail(request, pk):
+    """Render details of a specific recipe."""
     # Fetch a specific recipe using its primary key
     recipe = get_object_or_404(Recipe, pk=pk)
     return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
